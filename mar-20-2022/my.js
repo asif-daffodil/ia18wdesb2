dfa7e972b40dfdadc134640438dffcfa29cee510
Asif Abir <abir@dipti.com.bd>
2022-03-20 12:50:48 +0600

    mar-20-2022

---
+++ mar-20-2022/my.js
@@ -0,0 +1,79 @@
+// console.log("This is a demo text");
+// console.log("This is another text");
+
+/*
+    this is
+    a
+    multiline
+    comment
+*/
+
+var kamal = 123;
+// console.log(kamal);
+
+//data types
+var mishu = "Mishu lives in Norail";
+var eva = 12.3;
+var nila = null;
+var harun = true;
+// console.log(typeof mishu, typeof eva, typeof nila, typeof harun);
+
+//operator
+
+/*
+Arethmetic operator
++, -, *, /, %
+
+Assignment Operator
+=
++=
+-= 
+*= 
+/=
+%=
+
+increament/decrement operator
+++,
+--
+
+conditional operaotr
+==
+===
+<
+>
+<=
+>=
+!=
+!==
+
+logical operator
+&&
+||
+
+concatination
++
+
+*/
+
+var boyos = 20;
+boyos += 1;
+boyos -= 1;
+boyos *= 2;
+boyos /= 4;
+boyos %= 7;
+++boyos;
+boyos--;
+
+// console.log(boyos);
+
+mishu = 20;
+harun = "20";
+nila = 19;
+
+if (mishu <= harun || mishu < nila) {
+  console.log("Joy Bangla");
+}
+
+var php = "PHP is a server-side language";
+var JavaScript = "JS is a client-side language";
+console.log(php + JavaScript);
